d6f16d0af6a18a558c40930cb031a8a4555e34d0
philipp-fvef <entressfue@uni-potsdam.de>
2023-10-28 09:27:03 +0200

    playground inserted

---
+++ playground.py
@@ -0,0 +1,7 @@
+sentence = ["Hans", "isst", "ein", "Kaesebrot"]
+
+print("True  - S  ", sentence[0:4])
+print("True  - NP ", sentence[2:4])
+print("False - N  ", sentence[0:1])
+print("True  - VP ", sentence[1:4])
+print("False - V  ", sentence[0:1])
--- rd.py
+++ rd.py
@@ -3,24 +3,30 @@
 
 
 def parse_S(sentence: list, start: int, end: int) -> bool:
+    """docstring"""
     pass
 
 
 def parse_NP(sentence: list, start: int, end: int) -> bool:
+    """docstring"""
     pass
 
 
 def parse_VP(sentence: list, start: int, end: int) -> bool:
+    """docstring"""
     pass
 
 
 def parse_N(sentence: list, start: int, end: int) -> bool:
+    """docstring"""
     pass
 
 
 def parse_V(sentence: list, start: int, end: int) -> bool:
+    """docstring"""
     pass
 
 
 def parse_Det(sentence: list, start: int, end: int) -> bool:
+    """docstring"""
     pass
